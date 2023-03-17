a354a38679d948942e3769c6640ae4268d781952
Christian Kongsgaard <chrk@arkitema.com>
2023-03-17 12:37:58 +0100

    made databases an extra

--- src/lcacollect_config/fastapi.py
+++ src/lcacollect_config/fastapi.py
@@ -1,8 +1,14 @@
 from fastapi import Depends, Security
 
-from lcacollect_config.connection import get_db
 from lcacollect_config.security import azure_scheme
 
+try:
+    from lcacollect_config.connection import get_db
 
-async def get_context(session=Depends(get_db), user=Security(azure_scheme)):
-    return {"session": session, "user": user}
+    async def get_context(session=Depends(get_db), user=Security(azure_scheme)):
+        return {"session": session, "user": user}
+
+except (ImportError, ModuleNotFoundError):
+
+    async def get_context(user=Security(azure_scheme)):
+        return {"user": user}
