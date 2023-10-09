5db0ce9c2dc0b34dc7c6132cf8b2ef1ebe5700c1
Christian Kongsgaard <chrk@arkitema.com>
2023-10-09 10:39:11 +0200

    added option to filter on booleans

--- src/lcacollect_config/graphql/input_filters.py
+++ src/lcacollect_config/graphql/input_filters.py
@@ -18,6 +18,7 @@ class FilterOptions:
     is_empty: Optional[bool] = None
     is_not_empty: Optional[bool] = None
     is_any_of: Optional[list[str]] = None
+    is_: Optional[bool] = strawberry.field(name="is", default=None)
     json_contains: Optional[str] = None
 
 
@@ -39,6 +40,8 @@ def filter_model_query(model: SQLModelMetaclass, filters: BaseFilter, query: Opt
 
         if _filter.equal:
             query = query.where(field == _filter.equal)
+        elif _filter.is_:
+            query = query.where(field == _filter.is_)
         elif _filter.contains:
             query = query.where(col(field).contains(_filter.contains))
         elif _filter.starts_with:
